8661c1f570e0579711444888dcfd733c0cd91ef5
Mahmoud Elmaghraby <mahmoud.elmaghraby11@gmil.com>
2023-08-25 03:18:22 +0300

    making logo scrolling to top

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,14 @@
 import stickyNotes from '../../assets/images/sticky-note.png'
+const scrollToBtnHandler = ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+}
 const Header = ()=>{
     return (
         <div className="sticky top-0 bg-slate-950 p-4 text-center mb-2 text-white flex justify-between">
-            <a href="/sticky-notes"><img src={stickyNotes} width={50} alt="sticky notes logo" /></a>
+            <img onClick={scrollToBtnHandler} className='cursor-pointer hover:opacity-75' src={stickyNotes} width={50} alt="sticky notes logo" />
             <div>
                 <div className="header-title text-3xl font-bold font-righteous">Sticky Notes</div>
                 <div className="header-subtitle font-semibold font-plusJakarta">Expose your thoughts here.</div>
